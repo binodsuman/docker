7bcc8890ee1b3e1b1c7c5cad4fac0a1736f237d6
root <binodkumarsuman@gmail.com>
2017-05-11 08:25:19 +0530

    Third Commit to java file now

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ class HelloWorld{
         System.out.println("*************************************************************************");
         System.out.println("Congratulaton, your first java code from Docker Container Continuous Integraion");
 	System.out.println("Hello World  From Docker Container");
+	System.out.println("Third Commit to check");
         System.out.println("*************************************************************************");
 
  }
