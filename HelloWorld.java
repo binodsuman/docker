98d46761c6d6a096613944462ed4334a1c44c067
root <binodkumarsuman@gmail.com>
2017-05-11 08:11:22 +0530

    Java File Name typo correction

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld{
+
+ public static void main(String[] args){
+        System.out.println("*************************************************************************");
+        System.out.println("Congratulaton, your first java code from Docker Container Continuous Integraion");
+	System.out.println("Hello World  From Docker Container");
+        System.out.println("*************************************************************************");
+
+ }
+
+
+}
